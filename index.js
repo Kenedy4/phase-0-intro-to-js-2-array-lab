82f1ff2fd7474a76fde91ab8fc29599aac4b96ad
kenedy4 <kennedymusyoki8@gmail.com>
2024-06-30 19:18:57 +0300

    Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Write your solution here!
+
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+module.exports = {
+  destructivelyPrependCat: destructivelyPrependCat,
+};
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+module.exports = {
+  destructivelyAppendCat: destructivelyAppendCat,
+};
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+module.exports = {
+  destructivelyRemoveLastCat: destructivelyRemoveLastCat,
+};
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+module.exports = {
+  appendCat: appendCat,
+};
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+module.exports = {
+  prependCat: prependCat,
+};
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+module.exports = {
+  removeLastCat: removeLastCat,
+};
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+module.exports = {
+  removeFirstCat: removeFirstCat,
+};
